07034e79f892e5dd16917f0958bade06d4d4b6d9
Yesdoing <fkrmtleo12@gmail.com>
2021-09-29 17:23:05 +0900

    [feature] add splash image

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import Router from './src/navigation';
 import {setGlobalProps} from './src/utils/globalProps';
 setGlobalProps(); // set global props
 
-const App = () => <Router />;
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+  return <Router />;
+};
 
 export default App;
